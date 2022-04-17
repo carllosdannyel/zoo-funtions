a9ae8d7c5bb8e7dede45c4f79aca3e3f45a2fca4
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-04-17 13:58:52 -0300

    requisito 4 concluído

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+const { employees } = data;
+
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
+function isManager(ids) {
+  const managers = employees
+    .filter(({ id }) => id === stephanieId
+    || id === olaId
+    || id === burlId);
+
+  return managers.some(({ id }) => id === ids);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const employeesOfManager = employees.filter(({ managers }) => [...managers].includes(managerId));
+
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  try {
+    isManager(managerId);
+    return employeesOfManager.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  } catch (error) {
+    throw error.message;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
