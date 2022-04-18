00c82ed79b1dc5e73beef19a6c349813a74b09cf
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-04-18 17:58:53 -0300

    requisito 6 concluído

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { child, adult, senior } = data.prices;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const related = { adult: 0, child: 0, senior: 0 };
+
+  entrants.filter(({ age }) => {
+    if (age < 18) {
+      related.child += 1;
+    } else if (age >= 18 && age < 50) {
+      related.adult += 1;
+    } else {
+      related.senior += 1;
+    }
+    return related;
+  });
+
+  return related;
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = {}) {
+  if (Object.keys(entrants).length === 0 || !entrants) return 0;
+
+  const { adult: x, child: y, senior: z } = countEntrants(entrants);
+  const totalPrice = adult * x + child * y + senior * z;
+
+  return totalPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
