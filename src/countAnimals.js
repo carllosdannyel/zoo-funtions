ac7a673a343b11c721c6e928c87a2adf2792baaa
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-04-18 13:08:33 -0300

    requisito 5 concluído

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const animalCharacteristics = { ...animal };
+  if (!animal) {
+    return species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+  }
+  const animalsForSpecie = species.find(({ name }) =>
+    animalCharacteristics.specie.includes(name));
+  const animalsForSex = animalsForSpecie.residents.reduce((acc, { sex }) => {
+    if (animalCharacteristics.sex === sex) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+  return animalCharacteristics.specie && animalCharacteristics.sex
+    ? animalsForSex : animalsForSpecie.residents.length;
 }
 
 module.exports = countAnimals;
